84c7b0dbe1a042e6c1f5547efdd27e5546c2dbfc
Ami Patel <ahpatel@andrew.cmu.edu>
2023-10-30 12:29:17 -0700

    updating the scripts to serve client and app on render

--- postinstall.sh
+++ postinstall.sh
@@ -3,5 +3,5 @@ CURRENT_DIR=$(pwd)
 FRONTEND_DIR="${CURRENT_DIR}/client"
 
 cd "${FRONTEND_DIR}" && npm install
-cd "${FRONTEND_DIR}" && npm run start
+cd "${FRONTEND_DIR}" && npm run build
 cd "${CURRENT_DIR}"
